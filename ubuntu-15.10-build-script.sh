ddf26cedcb4ffe81e4c0be1a1525692dd57048ba
lukas <lukas@venezilu.de>
2016-02-08 17:40:48 +0100

    added additional dependency to build script

--- ubuntu-15.10-build-script.sh
+++ ubuntu-15.10-build-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install -y git autoconf libtool curl python3-pyqt5.* python3-numpy python3-serial python3-opengl python3-setuptools python3-dev qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-dialogs qml-module-qtquick-controls zlib1g build-essential cmake
+sudo apt-get install -y git autoconf libtool curl python3-pyqt5.* python3-numpy python3-serial python3-opengl python3-setuptools python3-sip-dev python3-dev qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-dialogs qml-module-qtquick-controls zlib1g build-essential cmake
 mkdir build && cd build
 
 cmake ..
